6fcb40c048aff6aade29c235f71e7aa2a824a9ea
grzegorzPL <grzegorz.laszczak@gmail.com>
2020-06-21 17:11:12 +0200

    Initial commit

---
+++ src/test/java/com/grzegorzlaszczak/mycrudapp/MyCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grzegorzlaszczak.mycrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
